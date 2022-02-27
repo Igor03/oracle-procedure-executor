e6b2326e62a004e31b07862526bf27844476932c
José Igor <j.igor.ifce@gmail.com>
2022-02-27 15:32:27 -0300

    Adding new structures

---
+++ JIgor.Projects.OracleProcedureExecutor.Samples/models/Output/OutputHeader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace JIgor.Projects.OracleProcedureExecutor.Samples.models.Output
+{
+    public class OutputHeader
+    {
+        public int Id { get; set; }
+        
+        public decimal TaxRate { get; set; }
+        
+        public decimal TotalAmount { get; set; }
+        
+        public IEnumerable<OutputItemCalculation> ItemsCalculation { get; set; }
+    }
+}
