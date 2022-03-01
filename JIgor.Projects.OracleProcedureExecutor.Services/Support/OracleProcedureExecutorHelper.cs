a0ec78daf208d55da47dcd6938debb1b80b381c8
José Igor <j.igor.ifce@gmail.com>
2022-03-01 16:20:39 -0300

    Modifying existent structures

---
+++ JIgor.Projects.OracleProcedureExecutor.Services/Support/OracleProcedureExecutorHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace JIgor.Projects.OracleProcedureExecutor.Services.Support
+{
+    public static  class OracleProcedureExecutorHelper
+    {
+        public static int[] GenerateArrayBindSize(int arraySize, int bindSize)
+        {
+            var arrayBindSize = new int[arraySize];
+            
+            for (var i = 0; i < arraySize; i++)
+            {
+                arrayBindSize[i] = bindSize;
+            }
+            return arrayBindSize;
+        }
+
+        public static OracleParameter CreateOracleParameter(
+            string name, 
+            OracleDbType type,
+            ParameterDirection direction,
+            object value = default)
+        {
+            var parameter = new OracleParameter()
+            {
+                ParameterName = name,
+                OracleDbType = type,
+                Direction = direction,
+                Value = value,
+            };
+
+            return parameter;
+        }
+
+        public static OracleParameter CreateArrayOracleParameter(
+            string name, 
+            ParameterDirection direction,
+            int size, 
+            int bindSize = default,
+            object value = default)
+        {
+            var parameter = new OracleParameter()
+            {
+                ParameterName = name,
+                Direction = direction,
+                CollectionType = OracleCollectionType.PLSQLAssociativeArray,
+                Size = size,
+                ArrayBindSize = GenerateArrayBindSize(size, bindSize),
+                Value = value
+            };
+
+            return parameter;
+        }
+    }
+}
---
+++ JIgor.Projects.OracleProcedureExecutor.Services/Support/ProcedureMetadata.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace JIgor.Projects.OracleProcedureExecutor.Services.Support
+{
+    public class ProcedureMetadata
+    {
+        public ProcedureMetadata(string name, string schema)
+        {
+            this.Name = name;
+            this.Schema = schema;
+            this.Parameters = new List<OracleParameter>();
+        }
+        
+        public string Name { get; set; }
+        
+        public string Schema { get; set; }
+
+        public List<OracleParameter> Parameters { get; set; }
+
+        public OracleParameter[] GetOutputParameters()
+        {
+            var outputParameters = new List<OracleParameter>();
+
+            this.Parameters.ForEach(p =>
+            {
+                if (p.Direction == ParameterDirection.Output)
+                {
+                    outputParameters.Add(p);
+                }
+            });
+            
+            return outputParameters.ToArray();
+        }
+
+        public void AddParameter(OracleParameter parameter)
+        {
+            _ = parameter ?? throw new ArgumentNullException(nameof(parameter));
+            this.Parameters.Add(parameter);
+        }
+
+        public string ToSqlStatement()
+        {
+            var sqlStatement = string.Empty;
+            this.Parameters.ForEach(p => sqlStatement += $":{p.ParameterName},");
+            return $"BEGIN {this.Schema}" + $".{this.Name}({sqlStatement.Remove(sqlStatement.Length - 1, 1)}); END;";
+        }
+    }
+}
